a4bea93a0d09ccea13a1d85abb3fb48ce55f4e2a
erichake <erichake@erichake-iMac>
2015-12-30 20:23:44 +0100

    add utf8 things for opening new figures

--- scripts/ControlPanel/ControlPanel.js
+++ scripts/ControlPanel/ControlPanel.js
@@ -201,66 +201,7 @@ function ControlPanel(_canvas) {
         }
     };
 
-    var Utf8 = {
-
-        // public method for url encoding
-        encode: function(string) {
-
-            var utftext = "";
-
-            for (var n = 0; n < string.length; n++) {
-
-                var c = string.charCodeAt(n);
-
-                if (c < 128) {
-                    utftext += String.fromCharCode(c);
-                } else if ((c > 127) && (c < 2048)) {
-                    utftext += String.fromCharCode((c >> 6) | 192);
-                    utftext += String.fromCharCode((c & 63) | 128);
-                } else {
-                    utftext += String.fromCharCode((c >> 12) | 224);
-                    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                    utftext += String.fromCharCode((c & 63) | 128);
-                }
-
-            }
-
-            return utftext;
-        },
-
-        // public method for url decoding
-        decode: function(utftext) {
-            var string = "";
-            var i = 0;
-            var c = c1 = c2 = 0;
-
-            while (i < utftext.length) {
-
-                c = utftext.charCodeAt(i);
-
-                if (c < 128) {
-                    string += String.fromCharCode(c);
-                    i++;
-                } else if ((c > 191) && (c < 224)) {
-                    c2 = utftext.charCodeAt(i + 1);
-                    string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                    i += 2;
-                } else {
-                    c2 = utftext.charCodeAt(i + 1);
-                    c3 = utftext.charCodeAt(i + 2);
-                    string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                    i += 3;
-                }
-
-            }
-
-            return string;
-        }
-
-    }
-
-
-
+    
     var downloadProc = function() {
         filepicker.pick({
                 extensions: ['.txt', '.dgp'],
@@ -269,7 +210,7 @@ function ControlPanel(_canvas) {
             },
             function(FPFile) {
                 filepicker.read(FPFile, function(data) {
-                    canvas.OpenFile("", Utf8.decode(data));
+                    canvas.OpenFile("", $U.utf8_decode(data));
                     if ($FPICKERFRAME !== null) {
                         $FPICKERFRAME.close();
                         $FPICKERFRAME = null;
--- scripts/Utils.js
+++ scripts/Utils.js
@@ -593,6 +593,49 @@ $U.leaveAccents = function(s) {
 //    return r;
 //};
 
+$U.utf8_encode = function(string) {
+    var utftext = "";
+    for (var n = 0; n < string.length; n++) {
+        var c = string.charCodeAt(n);
+        if (c < 128) {
+            utftext += String.fromCharCode(c);
+        } else if ((c > 127) && (c < 2048)) {
+            utftext += String.fromCharCode((c >> 6) | 192);
+            utftext += String.fromCharCode((c & 63) | 128);
+        } else {
+            utftext += String.fromCharCode((c >> 12) | 224);
+            utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+            utftext += String.fromCharCode((c & 63) | 128);
+        }
+    }
+    return utftext;
+};
+
+$U.utf8_decode = function(utftext) {
+    var string = "";
+    var i = 0;
+    var c = c1 = c2 = 0;
+    while (i < utftext.length) {
+        c = utftext.charCodeAt(i);
+        if (c < 128) {
+            string += String.fromCharCode(c);
+            i++;
+        } else if ((c > 191) && (c < 224)) {
+            c2 = utftext.charCodeAt(i + 1);
+            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+            i += 2;
+        } else {
+            c2 = utftext.charCodeAt(i + 1);
+            c3 = utftext.charCodeAt(i + 2);
+            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            i += 3;
+        }
+    }
+    return string;
+};
+
+
+
 $U.base64_encode = function(_data) {
     var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
     var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
@@ -650,6 +693,7 @@ $U.base64_decode = function(_data) {
         }
     } while (i < _data.length);
     dec = tmp_arr.join('');
+    dec = $U.utf8_decode(dec);
     return dec;
 };
 
